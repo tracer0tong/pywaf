5d0678fa9253969ad5630fed90a4ead2cc3a71ab
John Doe <johndoe@example.com>
2015-03-19 16:39:53 +0300

    Added requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='RequestParser',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/tracer0tong/pywaf',
+    license='',
+    author='yleonychev',
+    author_email='',
+    description='Simple python waf'
+)
